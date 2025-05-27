a6cf2ddac950a8000c6236b89931579c5d4ee899
KalyaniRangani <kalyanireddyrangani@gmail.com>
2025-05-27 18:50:51 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,15 +2,3 @@ def contdown(repo)
 {
   git "https://github.com/IntelliqDevops/${repo}.git"
 }
-def contbuild()
-{
-  sh "mvn package"
-} 
-def contdeploy(jobname,ipadd,contxtpath)
-{
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipadd}:/var/lib/tomcat10/webapps/${contxtpath}.war"
-}  
-def conttest(jobname)
-{
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
-}

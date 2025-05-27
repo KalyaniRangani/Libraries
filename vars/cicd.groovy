5ea5e5cfb98ce6b7923abc0077dc1d96ed1f088e
KalyaniRangani <kalyanireddyrangani@gmail.com>
2025-05-27 19:21:08 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,7 @@ def contbuild()
 {
   sh 'mvn package'
 }
-
+def contdeploy(jobname,ipadd,contextpath)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipadd}:/var/lib/tomcat10/webapps/${contextpath}.war'
+}
